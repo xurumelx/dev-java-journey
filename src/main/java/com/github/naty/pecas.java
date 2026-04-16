b4b05e208d18fdd54c0bacd32a01ebee48940aa5
xurumelx <natalyoenningrosa05@gmail.com>
2026-04-16 11:39:15 -0300

    Removendo .idea do repositório

---
+++ src/main/java/com/github/naty/pecas.java
@@ -0,0 +1,27 @@
+package com.github.naty;
+
+import java.util.Scanner;
+
+public class pecas {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int id1, id2;
+    int num_pecas1, num_pecas2;
+    double valor_unit1, valor_unit2;
+
+    id1 = sc.nextInt();
+    num_pecas1 = sc.nextInt();
+    valor_unit1 = sc.nextDouble();
+
+    id2 = sc.nextInt();
+    num_pecas2 = sc.nextInt();
+    valor_unit2 = sc.nextDouble();
+
+    double total = (num_pecas1 * valor_unit1) + (num_pecas2 * valor_unit2);
+
+    System.out.printf("Valor a pagar: R$ %.2f%n", total);
+
+    sc.close();
+
+    }
+}
---
+++ src/main/java/com/github/naty/vetor.java
@@ -0,0 +1,51 @@
+package com.github.naty;
+
+import java.util.Arrays;
+
+public class vetor {
+    public static void main(String[] args) {
+
+        int[] ListaDeNumeros = {5, 99, 42, 17, 63, 8, 74, 29, 91, 12, 56, 3, 88, 34, 70};
+
+        // MÉDIA
+        int soma = 0;
+        for (int i = 0; i < ListaDeNumeros.length; i++){
+            soma += ListaDeNumeros[i];
+        }
+        double media = (double) soma / ListaDeNumeros.length;
+        System.out.printf("Média dos valores: %.2f%n", media);
+
+        // MAIOR VALOR
+        int maiorValor = ListaDeNumeros[0];
+        for(int y = 0; y < ListaDeNumeros.length; y++){
+            if(maiorValor < ListaDeNumeros[y]){
+                maiorValor = ListaDeNumeros[y];
+            }
+        }
+        System.out.println("Maior valor do vetor: " + maiorValor);
+
+        // MENOR VALOR
+        int menorValor = ListaDeNumeros[0];
+        for(int x = 0; x < ListaDeNumeros.length; x++){
+            if(menorValor > ListaDeNumeros[x]){
+                menorValor = ListaDeNumeros[x];
+            }
+        }
+        System.out.println("Menor valor: " + menorValor);
+
+        // MEDIANA
+        double mediana;
+        int tamanho = ListaDeNumeros.length;
+
+        Arrays.sort(ListaDeNumeros);
+        System.out.println("Vetor ordenado: " + Arrays.toString(ListaDeNumeros));
+
+        if(tamanho % 2 == 0){
+            mediana = (ListaDeNumeros[tamanho/2 - 1] + ListaDeNumeros[tamanho/2]) / 2.0;
+            System.out.println("Mediana par: " + mediana);
+        } else {
+            mediana = ListaDeNumeros[tamanho/2];
+            System.out.println("Mediana ímpar: " + mediana);
+        }
+    }
+}
